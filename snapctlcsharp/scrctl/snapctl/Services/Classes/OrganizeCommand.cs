f51f65f09e8e90166353e7b8cf098fd1910445bd
Tojan-Naiem <tojan050@gmail.com>
2026-01-30 01:36:53 +0200

    feat : add category class for adding the categories and create direcory

--- snapctlcsharp/scrctl/snapctl/Services/Classes/OrganizeCommand.cs
+++ snapctlcsharp/scrctl/snapctl/Services/Classes/OrganizeCommand.cs
@@ -2,8 +2,28 @@ public class OrganizeCommand : ICommand
 {
     public void Execute(string args)
     {
-        
+        string screenshotPath=args;
+        CreateDirectory(screenshotPath,"person");
+
         Console.WriteLine(args);
+    }
+    public void CreateDirectory(string path,string dirName)
+    {
+        string newDir=path+"/"+dirName;
+        if (Directory.Exists(newDir))
+        {
+            Console.WriteLine("Directory "+dirName+" already exists");
+            return;
+        }
+        DirectoryInfo di=Directory.CreateDirectory(newDir);
+        if (!Directory.Exists(newDir))
+        {
+            Console.WriteLine("Directory does not exists");
+            return;
+        }
+        Console.WriteLine("Successfully created Directory "+dirName+" !!");
+
+        
     }
 
 }
